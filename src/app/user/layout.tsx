200ed014d270f7f2396330d31d7c2ff249115e97
pujisubarkah <67720528+pujisubarkah@users.noreply.github.com>
2025-07-03 23:29:17 +0700

    Update layout.tsx

--- src/app/user/layout.tsx
+++ src/app/user/layout.tsx
@@ -1,21 +1,18 @@
-// app/admin/layout.tsx
-"use client"
+'use client'
 
-import { SidebarUser } from '@/components/SidebarUser'
+import SidebarUserWrapper from '@/components/SidebarUserWrapper'
 import Header from '@/components/header'
 import Footer from '@/components/footer'
-import { Toaster } from 'sonner'
 
 export default function AdminLayout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex min-h-screen bg-gray-50 text-gray-900">
-      <SidebarUser />
+      <SidebarUserWrapper />
       <div className="flex flex-col flex-1">
         <Header />
         <main className="flex-1 p-6 overflow-y-auto">{children}</main>
         <Footer />
       </div>
-      <Toaster richColors position="top-center" />
     </div>
   )
 }
